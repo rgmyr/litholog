093240f722f1b4834dc255b5025530176680efc0
rgmyr <ross.meyer@utexas.edu>
2020-01-15 15:12:59 -0700

    split out IO dev features

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 
 ### Overview
 
-`graphiclog` is a package-level extension of [striplog](https://github.com/agile-geoscience/striplog), with the interface and additional features geared toward facilitating machine learning experiments and quantitative analysis of stratigraphic log datasets.
+`graphiclog` is a package-level extension of [striplog](https://github.com/agile-geoscience/striplog), with an API and additional features that are geared toward facilitating machine learning experiments and quantitative analysis of stratigraphic log datasets.
 
 ### Data Structures
 
-The package provides two objects:
+The package provides two primary data structures:
 - `Bed(striplog.Interval)`
 - `BedSequence(striplog.Striplog)`
 
 ### Lithological Utilities
 
-We work a lot with grainsize, which are usually more convenient in `log2` or *Psi* units, than the original `mm` measurements.
+We work a lot with grainsizes, which are more convenient to work with in `log2` (a.k.a. *Psi*) units, rather than the original `mm` measurements.
---
+++ graphiclog/_io_dev.py
@@ -0,0 +1,113 @@
+"""
+IO classes & functions.
+
+This is an incomplete implemention of a more general/customizable
+implementation of data checking/pre-processing.
+"""
+import operator
+from abc import abstractmethod
+
+import pandas as pd
+
+from graphiclog import utils
+
+
+class BaseCheck():
+    """
+    Base class for `Check` objects, which check something and take an action if the check fails.
+
+    Parameters
+    ----------
+    check_fn : func
+        Function that should return True if the check passes, False if it doesnt.
+    action : func or Exception
+        Action to take when `check_fn` fails (returns False).
+        If function, will apply action to operand and return result.
+        If Exception, will raise the Exception.
+    """
+    def __init__(self, check_fn, action):
+        self.check_fn = check_fn
+        self.action = action
+
+    @property
+    @abstractmethod
+    def level(self):
+        raise NotImplementedError
+
+    def check(self, thing):
+        return self.check_fn(thing)
+
+    def apply(self, thing):
+        """
+        Application of `action` to a `thing` if `check_fn` returns False.
+        """
+        if not self.check(thing):
+            if isinstance(self.action, Exception):
+                raise self.action
+            else:
+                return self.action(thing)
+        else:
+            return thing
+
+
+class RowCheck(BaseCheck):
+    @property
+    def level(self):
+        return 'row'
+
+
+class TableCheck(BaseCheck):
+    @property
+    def level(self):
+        return 'group'
+
+
+class DataFrameChecker():
+    """
+    Reads csv table(s) and applies a list of `Checks`
+    """
+    def __init__(self, checks, df=None, **kwargs):
+        assert all(isinstance(chk, BaseCheck) for chk in checks), '`checks` must be an iterable of `Check`s'
+
+        self.row_checks = [c for c in checks if c.level is 'row']
+        self.group_checks = [c for c in checks if c.level is 'group']
+
+        if df:
+            if isinstance(df, pd.DataFrame):
+                self.df = df
+            else:
+                self.df = self.read(df, **kwargs)
+
+    @property
+    def has_df(self):
+        return isinstance(self.df, pd.DataFrame)
+
+
+    def read(self, fpath, converters={}, **kwargs):
+        try:
+            return pd.read_csv(df, converters=converters, **kwargs)
+        except UnicodeDecodeError:
+            kwargs['encoding'] = 'latin-1'
+            return pd.read_csv(df, converters=converters, **kwargs)
+
+
+    def add_check(self, check):
+        assert isinstance(check, BaseCheck), f'Can only add a `*Check`, not {type(check)}'
+        if c.level is 'row':
+            self.row_checks.append(check)
+        elif c.level is 'group':
+            self.group_checks.append(check)
+        else:
+            raise ValueError(f'Unknown `Check.level` value: {check.level}')
+
+
+    def split_by(self, field):
+        pass
+
+
+def fill_column_nan(df, col, fill_value, indicator='missing'):
+    """
+    Fill missing values in `col` with `fill_value`.
+    Add a new bool column '`indicator`_`col`'.
+    """
+    pass
